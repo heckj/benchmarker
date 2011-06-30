70978c8370a6d91478d67cd32fb4030b1a0bb77c
Joe Heck <heckj@mac.com>
2011-06-30 11:53:33 -0700

    switching out venv install

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -4,7 +4,7 @@
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
 #
-# Copyright 2010 OpenStack LLC.
+# Copyright 2010 OpenStack, LLC
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -19,7 +19,7 @@
 #    under the License.
 
 """
-Installation script for Benchmark's development virtualenv
+Installation script for the OpenStack Benchmarks development virtualenv.
 """
 
 import os
@@ -29,6 +29,7 @@ import sys
 
 ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 VENV = os.path.join(ROOT, '.benchmark-venv')
+WITH_VENV = os.path.join(ROOT, 'tools', 'with_venv.sh')
 PIP_REQUIRES = os.path.join(ROOT, 'tools', 'pip-requires')
 
 
@@ -37,7 +38,8 @@ def die(message, *args):
     sys.exit(1)
 
 
-def run_command(cmd, redirect_output=True, check_exit_code=True):
+def run_command(cmd, redirect_output=True, check_exit_code=True, cwd=ROOT,
+                die_message=None):
     """
     Runs a command in an out-of-process shell, returning the
     output of that command.  Working directory is ROOT.
@@ -47,90 +49,90 @@ def run_command(cmd, redirect_output=True, check_exit_code=True):
     else:
         stdout = None
 
-    proc = subprocess.Popen(cmd, cwd=ROOT, stdout=stdout)
+    proc = subprocess.Popen(cmd, cwd=cwd, stdout=stdout)
     output = proc.communicate()[0]
     if check_exit_code and proc.returncode != 0:
-        die('Command "%s" failed.\n%s', ' '.join(cmd), output)
+        if die_message is None:
+            die('Command "%s" failed.\n%s', ' '.join(cmd), output)
+        else:
+            die(die_message)
     return output
 
 
 HAS_EASY_INSTALL = bool(run_command(['which', 'easy_install'],
                                     check_exit_code=False).strip())
 HAS_VIRTUALENV = bool(run_command(['which', 'virtualenv'],
-                                    check_exit_code=False).strip())
+                                  check_exit_code=False).strip())
 
 
 def check_dependencies():
     """Make sure virtualenv is in the path."""
 
+    print 'checking dependencies...'
     if not HAS_VIRTUALENV:
         print 'not found.'
         # Try installing it via easy_install...
         if HAS_EASY_INSTALL:
             print 'Installing virtualenv via easy_install...',
-            if not run_command(['which', 'easy_install']):
-                die('ERROR: virtualenv not found.\n\n'
-                    'Glance development requires virtualenv, please install'
-                    ' it using your favorite package management tool')
-            print 'done.'
-    print 'done.'
+            run_command(['easy_install', 'virtualenv'],
+                        die_message='easy_install failed to install virtualenv'
+                                    '\ndevelopment requires virtualenv, please'
+                                    ' install it using your favorite tool')
+            if not run_command(['which', 'virtualenv']):
+                die('ERROR: virtualenv not found in path.\n\ndevelopment '
+                    ' requires virtualenv, please install it using your'
+                    ' favorite package management tool and ensure'
+                    ' virtualenv is in your path')
+            print 'virtualenv installation done.'
+        else:
+            die('easy_install not found.\n\nInstall easy_install'
+                ' (python-setuptools in ubuntu) or virtualenv by hand,'
+                ' then rerun.')
+    print 'dependency check done.'
 
 
 def create_virtualenv(venv=VENV):
-    """
-    Creates the virtual environment and installs PIP only into the
+    """Creates the virtual environment and installs PIP only into the
     virtual environment
     """
     print 'Creating venv...',
     run_command(['virtualenv', '-q', '--no-site-packages', VENV])
     print 'done.'
     print 'Installing pip in virtualenv...',
-    if not run_command(['tools/with_venv.sh', 'easy_install', 'pip']).strip():
+    if not run_command([WITH_VENV, 'easy_install', 'pip']).strip():
         die("Failed to install pip.")
     print 'done.'
 
 
 def install_dependencies(venv=VENV):
     print 'Installing dependencies with pip (this can take a while)...'
-
-    # Install greenlet by hand - just listing it in the requires file does not
-    # get it in stalled in the right order
-    venv_tool = 'tools/with_venv.sh'
-    run_command([venv_tool, 'pip', 'install', '-E', venv, '-r', PIP_REQUIRES],
+    run_command([WITH_VENV, 'pip', 'install', '-E', venv, '-r', PIP_REQUIRES],
                 redirect_output=False)
 
-    # Tell the virtual env how to "import glance"
-    pthfile = os.path.join(venv, "lib", "python2.6", "site-packages",
-                                 "glance.pth")
+    # Tell the virtual env how to "import benchmarks"
+    py = 'python%d.%d' % (sys.version_info[0], sys.version_info[1])
+    pthfile = os.path.join(venv, "lib", py, "site-packages", "benchmark.pth")
     f = open(pthfile, 'w')
     f.write("%s\n" % ROOT)
 
 
-def print_help():
-    help = """
- Benchmark development environment setup is complete.
+def print_summary():
+    summary = """
+ OpenStack Benchmarks development environment setup is complete.
 
- Benchmark development uses virtualenv to track and manage Python
- dependencies while in development and testing.
-
- To activate the benchmark virtualenv for the extent of your current
- shell session you can run:
+ To activate the virtualenv for the extent of your current shell session you
+ can run:
 
  $ source .benchmark-venv/bin/activate
-
- Or, if you prefer, you can run commands in the virtualenv on a case by case
- basis by running:
-
- $ tools/with_venv.sh <your command>
-    """
-    print help
+  """
+    print summary
 
 
-def main(argv):
+def main():
     check_dependencies()
     create_virtualenv()
     install_dependencies()
-    print_help()
+    print_summary()
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
