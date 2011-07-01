0b10d410f7b6a266bd551d5ac4b7dd41a19c4177
Joe Heck <heckj@mac.com>
2011-07-01 09:09:10 -0700

    adding in some documentation, refactoring a bit

--- bin/celery-benchmark.py
+++ bin/celery-benchmark.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python
+""" Benchmarking script to run celery benchmarks """
 
 import math
 import timeit
 from benchmark.celerybench.tasks import add
 
 def dumb_test_code():
+    """ my little 'how the hell does this work anyway' testing code.
+    Probably useless and should be deleted
+    """
     print "add.delay(4, 4)"
     result = add.delay(4, 4)
     print "result.ready() is ",result.ready()
     print "result.get() ", result.get() #wait for the result
     print "result.successful() is ",result.successful()
     print "and the result is ", result.result
+    print "delivery_mode is ",add.delivery_mode
 
+    print "delivery_mode is ",add.delivery_mode
     print "add.apply_async(args=[4, 4])"
     result = add.apply_async(args=[4, 4])
     print "result.ready() is ",result.ready()
     print "result.get() ", result.get() #wait for the result
     print "result.successful() is ",result.successful()
     print "and the result is ", result.result
+    print "delivery_mode is ",add.delivery_mode
 
-def do_work():
+def do_work(wait_for_result=False):
+    """ here's our magic function that inserts into the message
+    queue, does something, and optionally waits for a result.
+    Highly dependent on the 
+    import benchmark.celerybench.tasks module """ 
     result = add.apply_async(args=[4, 4])
-    result.get() #wait for the result
+    if wait_for_result:
+        result.get() #block and wait for the result
 
 def calculate_results(measurements):
     """Take a list of measurements and calculate results from it.
     Results:
     (minimum, mean, maximum, std. deviation, median, 90th percentile)
+    This whole segment borrowed from 
+    http://www.testing-software.org/Tools/timeit/
     """
 
     def calculate_std(measurements, mean):
@@ -65,8 +79,19 @@ def calculate_results(measurements):
     p90 = calculate_percentile(measurements, 0.9)
     return minimum, mean, maximum, std, median, p90
 
-def run_benchmark(iterations=100):
-    t = timeit.Timer(stmt='do_work()',
+def run_benchmark(iterations=100, roundtrip=False):
+    """ run the benchmarks with timeit - using the do_work method
+    defined above. This is setup to prodigously print, and be able
+    to be run in chunks. i.e.
+
+    for chunk in range(1,1000):
+        (measurements,result_set) = run_benchmark(iterations=100)
+        append_results(measurements, result_set) 
+    """
+    command_to_run = "do_work()"
+    if roundtrip:
+        command_to_run = "do_work(wait_for_result=True"
+    t = timeit.Timer(stmt=command_to_run,
                      setup='from __main__ import do_work; gc.enable()')
     # run the timer against a single stmt loop 100 times
     measurements = t.repeat(iterations, 1)
@@ -80,8 +105,17 @@ def run_benchmark(iterations=100):
     print "std: %.2f ms" % (result_set[3]*1000, )
     print "median: %.2f ms" % (result_set[4]*1000, )
     print "p90: %.2f ms" % (result_set[5]*1000, )
+    return (measurements, result_set)
+
+def start_results_log():
+    """ start writing a benchmark results output file """
     FOUT = open("benchmark_results.txt", "w")
     FOUT.write("measurements\tmin\tmean\tman\tstddev\tmedian\tp90\n")
+    FOUT.close()
+
+def append_results(measurements, result_set):
+    """ append a chunk a results to the output file """
+    FOUT = open("benchmark_results.txt", "a")
     stringout = "%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (len(measurements),
                                                result_set[0],
                                                result_set[1],
@@ -93,4 +127,9 @@ def run_benchmark(iterations=100):
     FOUT.close()
 
 if __name__ == '__main__':
-    run_benchmark(iterations=1000)
+    #dumb_test_code()
+    start_results_log()
+    for chunk in range(1,1000):
+        (measurements,result_set) = run_benchmark(iterations=100)
+        append_results(measurements, result_set) 
+    print "FINI!"
