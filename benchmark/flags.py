fadbb80a855b1da9853116afae392a7f4be7f212
Joe Heck <heckj@mac.com>
2011-07-11 16:32:01 -0700

    adding additional basic metrics for benchmarking

--- benchmark/flags.py
+++ benchmark/flags.py
@@ -4,4 +4,6 @@ FLAGS = gflags.FLAGS
 
 gflags.DEFINE_string('graphite_server', '192.168.1.121', 'host running graphite and pystatsd service')
 gflags.DEFINE_integer('graphite_port', 2003, 'graphite/(py)statsd port to send data into')
+gflags.DEFINE_integer('benchagent_interval', 10, 'interval in which benchagent sends in metrics')
+
 #gflags.DEFINE_boolean('debug', False, 'produce debugging output')
