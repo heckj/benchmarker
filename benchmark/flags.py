5a05122f9e8e086464cfc2cf1fbaac64fe362d9f
Joe Heck <heckj@mac.com>
2011-07-11 16:08:21 -0700

    switching to straight socket() connection to Graphite

--- benchmark/flags.py
+++ benchmark/flags.py
@@ -2,6 +2,6 @@ import gflags
 
 FLAGS = gflags.FLAGS
 
-gflags.DEFINE_string('graphite_server', '192.168.1.18', 'host running graphite and pystatsd service')
-gflags.DEFINE_integer('graphite_port', 8125, 'graphite/(py)statsd port to send data into')
+gflags.DEFINE_string('graphite_server', '192.168.1.121', 'host running graphite and pystatsd service')
+gflags.DEFINE_integer('graphite_port', 2003, 'graphite/(py)statsd port to send data into')
 #gflags.DEFINE_boolean('debug', False, 'produce debugging output')
